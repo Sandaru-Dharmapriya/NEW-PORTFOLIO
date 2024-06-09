9ea2eb73aaee6a28dece0fad92868abdb73ba7ec
Sandaru Dharmapriya <148580595+Sandaru-Dharmapriya@users.noreply.github.com>
2024-06-09 14:47:49 +0530

    1

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css:{
-    modules:{
+  base: '.', // Add this line with your repository name
+  css: {
+    modules: {
       localsConvention: "camelCase"
     },
   },
