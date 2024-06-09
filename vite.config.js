24f16add824d3e1152cf14b4f3143fd810624c50
Sandaru Dharmapriya <148580595+Sandaru-Dharmapriya@users.noreply.github.com>
2024-06-09 14:50:31 +0530

    2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '.', // Add this line with your repository name
+  base: 'NEW-PORFOLIO', // Add this line with your repository name
   css: {
     modules: {
       localsConvention: "camelCase"
